3d4578060d3268570986448b99ac32c3059b8e15
RishiMenon2004 <rishimenon82004@gmail.com>
2026-06-07 19:09:17 +0530

    Initial Commit - Baseline setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
+// import { Geist } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+/* Keeping for future ref */
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,8 +20,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={''}>
+      <body>
+        <Sidebar/>
+        {children}</body>
     </html>
+  );
+}
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h1>Chiz.pink</h1>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/checklist">Checklist</Link>
+        <Link href="/characters">Characters</Link>
+        <Link href="/inventory">Inventory</Link>
+      </nav>
+    </div>
   );
 }
