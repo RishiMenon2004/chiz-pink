5a69469882e50d2ce3f07d03decd3a7dadb5eb52
RishiMenon2004 <rishimenon82004@gmail.com>
2026-06-19 19:56:27 +0530

    added arcs page

---
+++ src/app/arcs/page.tsx
@@ -0,0 +1,10 @@
+import routes from "@/database/routes";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: routes["/arcs"].head,
+};
+
+export default function Characters() {
+	
+}
--- src/database/routes.ts
+++ src/database/routes.ts
@@ -19,6 +19,12 @@ const routes: RouteData = {
 		id: "characters"
 	},
 
+	"/arcs": {
+		title: "ARCS",
+		head: "Arcs",
+		id: "arcs"
+	},
+
 	"/inventory": {
 		title: "INVENTORY",
 		head:"Inventory",
