d75362920a32e7a9188c7c25b7e50f4e8ad57074
RishiMenon2004 <rishimenon82004@gmail.com>
2026-06-28 01:50:39 +0530

    make tooltips flip sides when overflowing out of the screen

--- src/components/layout/TooltipContainer.tsx
+++ src/components/layout/TooltipContainer.tsx
@@ -1,12 +1,39 @@
-import { ReactNode, useEffect, useRef } from "react";
+import { ReactNode, useEffect, useRef } from "react"
 
-export function TooltipContainer({ subtext, startingPos, offset, children }: { subtext?: string, startingPos: { x: number, y: number }, offset: { x: number, y: number }, children: ReactNode }) {
+export function TooltipContainer({
+	subtext,
+	startingPos,
+	offset,
+	children,
+}: {
+	subtext?: string
+	startingPos: { x: number; y: number }
+	offset: { x: number; y: number }
+	children: ReactNode
+}) {
 	const tooltipRef = useRef<HTMLDivElement>(null!)
 
 	useEffect(() => {
 		const handleMouseMove = (e: globalThis.MouseEvent) => {
 			if (tooltipRef.current) {
-				const mousePos = { x: e.clientX + (offset.x || 0), y: e.clientY + (offset.y || 0) - tooltipRef.current.clientHeight }
+				const mousePos = {
+					x: e.clientX + (offset.x || 0),
+					y:
+						e.clientY +
+						(offset.y || 0) -
+						tooltipRef.current.clientHeight,
+				}
+
+				if (
+					mousePos.x + tooltipRef.current.clientWidth >
+					document.body.clientWidth
+				) {
+					mousePos.x =
+						e.clientX -
+						(offset.x / 2 || 0) -
+						tooltipRef.current.clientWidth
+				}
+
 				tooltipRef.current.style.transform = `translate(${mousePos.x}px, ${mousePos.y}px)`
 			}
 		}
@@ -19,8 +46,10 @@ export function TooltipContainer({ subtext, startingPos, offset, children }: { s
 		tooltipRef.current.style.transform = `translate(${startingPos.x}px, ${startingPos.y}px)`
 	}, [startingPos])
 
-	return <div className="tooltip" ref={tooltipRef}>
-		{children}
-		{subtext && <div className="subtext">{subtext}</div>}
-	</div>
+	return (
+		<div className="tooltip" ref={tooltipRef}>
+			{children}
+			{subtext && <div className="subtext">{subtext}</div>}
+		</div>
+	)
 }
